4e5d82e5d9088a80cced0f5345c13fdab8bcc15e
amitay <amitay1599@gmail.com>
2022-10-30 12:36:11 +0100

    deleteing try

--- back/server.js
+++ back/server.js
@@ -18,9 +18,7 @@ app.use((req, res, next) => {
   next()
 })
 
-app.get('/', (req, res) => { 
-  res.status(200).json({"tyrrrrr": "holaaaa!!!!!!"})
-})
+
 // routes
 app.use('/api/challenges', challengeRoutes)
 app.use('/api/user', userRoutes)
