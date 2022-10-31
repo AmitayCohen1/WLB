f55b7ac0062dd4665db244618805a121f86f7c14
amitay <amitay1599@gmail.com>
2022-10-31 12:14:08 +0100

    -

--- front/src/components/Parents.js
+++ front/src/components/Parents.js
@@ -48,6 +48,7 @@ const Parents = ({ challenge }) => {
                    
         <div  className='bg-green-200 object-cover rounded-xl aspect-video h-full w-full'>
             <ReactPlayer 
+            className='react-player'
             width='100%'
             height='100%'
             url={challenge.fileURL}
