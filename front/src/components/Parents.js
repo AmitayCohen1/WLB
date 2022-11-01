662f39009e5ee6d5e5abc4247d879040ec5a98bf
amitay <amitay1599@gmail.com>
2022-11-01 01:59:13 +0100

    reactplayer

--- front/src/components/Parents.js
+++ front/src/components/Parents.js
@@ -49,8 +49,6 @@ const Parents = ({ challenge }) => {
                 <div className='h-40 w-40 rounded-xl'>
                     <ReactPlayer 
                     className="react-player"
-                        width='100%'
-                        height='100%'
                         style={{
                             objectFit: 'cover'
                         }}
