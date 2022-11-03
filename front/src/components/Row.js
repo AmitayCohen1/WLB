2fce0d19fa710e371748b2d97e03cc9bcbe0d412
amitay <amitay1599@gmail.com>
2022-11-03 14:43:27 +0100

    rows thin

--- front/src/components/Row.js
+++ front/src/components/Row.js
@@ -81,8 +81,8 @@ const Row = ({challenge,index, parent}) => {
             className='react-player'
             width='100%'
             height='100%' 
-            // url={challenge.fileURL}
-            url='https://www.youtube.com/watch?v=kCnNWyl9qSE&t=284s&ab_channel=KlasKarlsson'
+            url={challenge.fileURL}
+            // url='https://www.youtube.com/watch?v=kCnNWyl9qSE&t=284s&ab_channel=KlasKarlsson'
 
           />
           </div>
