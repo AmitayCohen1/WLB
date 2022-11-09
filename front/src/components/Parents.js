0e6441d0c50259d0d0338d29816e362cc39a3cab
amitay <amitay1599@gmail.com>
2022-11-09 20:35:34 +0200

    deleteing video controls

--- front/src/components/Parents.js
+++ front/src/components/Parents.js
@@ -68,7 +68,7 @@ const Parents = ({ challenge }) => {
                             <div className=' bg-white bg-opacity-25 rounded-xl overflow-hidden grayscale aspect-square
                             '>
                             <ReactPlayer 
-                            controls
+                           
                             className='react-player'
                                 width='100%'
                                 height='100%'
